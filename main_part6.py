866fe6d6b6296e822450b531a1f765e461859fba
Brillouin-Zone <45795787+Brillouin-Zone@users.noreply.github.com>
2021-04-30 18:08:10 +0200

    Upload
    line ratio plots including targets 88, 204, 435 and 22124,
    see: https://arxiv.org/pdf/1902.05960.pdf

---
+++ main_part6.py
@@ -0,0 +1,207 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+import scipy.optimize as sci
+from scipy import interpolate, integrate
+from scipy.integrate import simps
+from scipy.optimize import curve_fit
+from scipy.special import erf, erfc
+from scipy.interpolate import interp1d
+from scipy.optimize import fsolve
+import scipy.constants as const
+
+import astropy
+from astropy import units as u
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from astropy.modeling import models
+from astropy.modeling import fitting
+import scipy.ndimage as snd
+from astropy.io import fits as pyfits
+from astropy.nddata import Cutout2D
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+
+import statistics
+import random
+from decimal import Decimal
+from lmfit.model import Model, ModelResult
+from lmfit import Parameters, minimize, fit_report, Minimizer, report_fit
+import pandas as pd
+import seaborn as sns
+import time
+from datetime import timedelta
+
+start_time = time.monotonic()
+##########################################################################################################################
+# P A R T   6  : CREATE LINE-RATIO-PLOTS ACCORDING TO NANAYAKKARA FOR TARGETS 88, 204, 435, 22429 #########################
+        # (x,y) = (SiIII] / CIII] , CIII] / OIII])   and (OIII] / HeII, CIII] / HeII)
+        # latter one to distinguish between AGN- and stellar-ionizing photons
+
+# 1) extract data from tables created in main_part2
+###################################################
+NAME = 'EXTRACTION'
+iden_str = ['88', '204', '435', '22429'] # string version
+iden_int = [88, 204, 435, 22429] # integer version
+REDSHIFT = [2.9541607, 3.1357558, 3.7247474, 2.9297342]
+
+EXTRACTIONS_HeII = 'HeII_selected_ALL_%s.fits'%(NAME)
+extractions_HeII = pyfits.open(EXTRACTIONS_HeII)
+DATA_HeII = extractions_HeII[1].data
+
+EXTRACTIONS_CIII = 'CIII_selected_ALL_%s.fits'%(NAME)
+extractions_CIII = pyfits.open(EXTRACTIONS_CIII)
+DATA_CIII = extractions_CIII[1].data
+
+EXTRACTIONS_OIII = 'OIII_selected_ALL_%s.fits'%(NAME)
+extractions_OIII = pyfits.open(EXTRACTIONS_OIII)
+DATA_OIII = extractions_OIII[1].data
+
+EXTRACTIONS_SiIII = 'SiIII_selected_ALL_%s.fits'%(NAME)
+extractions_SiIII = pyfits.open(EXTRACTIONS_SiIII)
+DATA_SiIII = extractions_SiIII[1].data
+
+# 2) define variables
+###################################################
+rest_wavelen_HeII_88 = DATA_HeII.field('rest_wavelen_He-II_88')
+flux_HeII_88 = DATA_HeII.field('flux_He-II_88')
+noise_HeII_88 = DATA_HeII.field('noise_He-II_88')
+rest_wavelen_HeII_204 = DATA_HeII.field('rest_wavelen_He-II_204')
+flux_HeII_204 = DATA_HeII.field('flux_He-II_204')
+noise_HeII_204 = DATA_HeII.field('noise_He-II_204')
+rest_wavelen_HeII_435 = DATA_HeII.field('rest_wavelen_He-II_435')
+flux_HeII_435 = DATA_HeII.field('flux_He-II_435')
+noise_HeII_435 = DATA_HeII.field('noise_He-II_435')
+#rest_wavelen_HeII_538 = DATA_HeII.field('rest_wavelen_He-II_538')
+#flux_HeII_538 = DATA_HeII.field('flux_He-II_538')
+#noise_HeII_538 = DATA_HeII.field('noise_He-II_538')
+rest_wavelen_HeII_22429 = DATA_HeII.field('rest_wavelen_He-II_22429')
+flux_HeII_22429 = DATA_HeII.field('flux_He-II_22429')
+noise_HeII_22429 = DATA_HeII.field('noise_He-II_22429')
+#rest_wavelen_HeII_23124 = DATA_HeII.field('rest_wavelen_He-II_23124')
+#flux_HeII_23124 = DATA_HeII.field('flux_He-II_23124')
+#noise_HeII_23124 = DATA_HeII.field('noise_He-II_23124')
+rest_wavelen_HeII = [rest_wavelen_HeII_88, rest_wavelen_HeII_204, rest_wavelen_HeII_435, rest_wavelen_HeII_22429]
+flux_HeII = [flux_HeII_88, flux_HeII_204, flux_HeII_435, flux_HeII_22429]
+noise_HeII = [noise_HeII_88, noise_HeII_204, noise_HeII_435, noise_HeII_22429]
+
+rest_wavelen_CIII_88 = DATA_CIII.field('rest_wavelen_C-III_88')
+flux_CIII_88 = DATA_CIII.field('flux_C-III_88')
+noise_CIII_88 = DATA_CIII.field('noise_C-III_88')
+rest_wavelen_CIII_204 = DATA_CIII.field('rest_wavelen_C-III_204')
+flux_CIII_204 = DATA_CIII.field('flux_C-III_204')
+noise_CIII_204 = DATA_CIII.field('noise_C-III_204')
+rest_wavelen_CIII_435 = DATA_CIII.field('rest_wavelen_C-III_435')
+flux_CIII_435 = DATA_CIII.field('flux_C-III_435')
+noise_CIII_435 = DATA_CIII.field('noise_C-III_435')
+#rest_wavelen_CIII_538 = DATA_CIII.field('rest_wavelen_C-III_538')
+#flux_CIII_538 = DATA_CIII.field('flux_C-III_538')
+#noise_CIII_538 = DATA_CIII.field('noise_C-III_538')
+rest_wavelen_CIII_22429 = DATA_CIII.field('rest_wavelen_C-III_22429')
+flux_CIII_22429 = DATA_CIII.field('flux_C-III_22429')
+noise_CIII_22429 = DATA_CIII.field('noise_C-III_22429')
+#rest_wavelen_CIII_23124 = DATA_CIII.field('rest_wavelen_C-III_23124')
+#flux_CIII_23124 = DATA_CIII.field('flux_C-III_23124')
+#noise_CIII_23124 = DATA_CIII.field('noise_C-III_23124')
+rest_wavelen_CIII = [rest_wavelen_CIII_88, rest_wavelen_CIII_204, rest_wavelen_CIII_435, rest_wavelen_CIII_22429]
+flux_CIII = [flux_CIII_88, flux_CIII_204, flux_CIII_435, flux_CIII_22429]
+noise_CIII = [noise_CIII_88, noise_CIII_204, noise_CIII_435, noise_CIII_22429]
+
+rest_wavelen_OIII_88 = DATA_OIII.field('rest_wavelen_O-III_88')
+flux_OIII_88 = DATA_OIII.field('flux_O-III_88')
+noise_OIII_88 = DATA_OIII.field('noise_O-III_88')
+rest_wavelen_OIII_204 = DATA_OIII.field('rest_wavelen_O-III_204')
+flux_OIII_204 = DATA_OIII.field('flux_O-III_204')
+noise_OIII_204 = DATA_OIII.field('noise_O-III_204')
+rest_wavelen_OIII_435 = DATA_OIII.field('rest_wavelen_O-III_435')
+flux_OIII_435 = DATA_OIII.field('flux_O-III_435')
+noise_OIII_435 = DATA_OIII.field('noise_O-III_435')
+#rest_wavelen_OIII_538 = DATA_OIII.field('rest_wavelen_O-III_538')
+#flux_OIII_538 = DATA_OIII.field('flux_O-III_538')
+#noise_OIII_538 = DATA_OIII.field('noise_O-III_538')
+rest_wavelen_OIII_22429 = DATA_OIII.field('rest_wavelen_O-III_22429')
+flux_OIII_22429 = DATA_OIII.field('flux_O-III_22429')
+noise_OIII_22429 = DATA_OIII.field('noise_O-III_22429')
+#rest_wavelen_OIII_23124 = DATA_OIII.field('rest_wavelen_O-III_23124')
+#flux_OIII_23124 = DATA_OIII.field('flux_O-III_23124')
+#noise_OIII_23124 = DATA_OIII.field('noise_O-III_23124')
+rest_wavelen_OIII = [rest_wavelen_OIII_88, rest_wavelen_OIII_204, rest_wavelen_OIII_435, rest_wavelen_OIII_22429]
+flux_OIII = [flux_OIII_88, flux_OIII_204, flux_OIII_435, flux_OIII_22429]
+noise_OIII = [noise_OIII_88, noise_OIII_204, noise_OIII_435, noise_OIII_22429]
+
+rest_wavelen_SiIII_88 = DATA_SiIII.field('rest_wavelen_Si-III_88')
+flux_SiIII_88 = DATA_SiIII.field('flux_Si-III_88')
+noise_SiIII_88 = DATA_SiIII.field('noise_Si-III_88')
+rest_wavelen_SiIII_204 = DATA_SiIII.field('rest_wavelen_Si-III_204')
+flux_SiIII_204 = DATA_SiIII.field('flux_Si-III_204')
+noise_SiIII_204 = DATA_SiIII.field('noise_Si-III_204')
+rest_wavelen_SiIII_435 = DATA_SiIII.field('rest_wavelen_Si-III_435')
+flux_SiIII_435 = DATA_SiIII.field('flux_Si-III_435')
+noise_SiIII_435 = DATA_SiIII.field('noise_Si-III_435')
+#rest_wavelen_SiIII_538 = DATA_SiIII.field('rest_wavelen_Si-III_538')
+#flux_SiIII_538 = DATA_SiIII.field('flux_Si-III_538')
+#noise_SiIII_538 = DATA_SiIII.field('noise_Si-III_538')
+rest_wavelen_SiIII_22429 = DATA_SiIII.field('rest_wavelen_Si-III_22429')
+flux_SiIII_22429 = DATA_SiIII.field('flux_Si-III_22429')
+noise_SiIII_22429 = DATA_SiIII.field('noise_Si-III_22429')
+#rest_wavelen_SiIII_23124 = DATA_SiIII.field('rest_wavelen_Si-III_23124')
+#flux_SiIII_23124 = DATA_SiIII.field('flux_Si-III_23124')
+#noise_SiIII_23124 = DATA_SiIII.field('noise_Si-III_23124')
+rest_wavelen_SiIII = [rest_wavelen_SiIII_88, rest_wavelen_SiIII_204, rest_wavelen_SiIII_435, rest_wavelen_SiIII_22429]
+flux_SiIII = [flux_SiIII_88, flux_SiIII_204, flux_SiIII_435, flux_SiIII_22429]
+noise_SiIII = [noise_SiIII_88, noise_SiIII_204, noise_SiIII_435, noise_SiIII_22429]
+
+# 3) create the line-ratio plot (x,y) = (SiIII] / CIII], CIII] / OIII])
+#                               (x,y) = (OIII] / HeII, CIII] / HeII)
+#########################################################################
+index_max_flux_HeII = [np.argmax(flux_HeII[0]), np.argmax(flux_HeII[1]), 17, np.argmax(flux_HeII[3])]
+index_max_flux_CIII = [17, 19, np.argmax(flux_CIII[2]), np.argmax(flux_CIII[3])] # max element good choice for 435, 22429. For 88: third largest. Not sure about CIII]-peak for 204
+index_max_flux_OIII = [np.argmax(flux_OIII[0]), np.argmax(flux_OIII[1]), 15, 12] # TODO: not sure about peak locations
+index_max_flux_SiIII = [np.argmax(flux_SiIII[0]), 23, 14, np.argmax(flux_SiIII[3])] # TODO: not sure about existence of these peaks
+
+SiC = [0, 0, 0, 0]
+CO = [0, 0, 0, 0]
+OHe = [0, 0, 0, 0]
+CHe = [0, 0, 0, 0, 0]
+for i in range(len(index_max_flux_HeII)):
+    SiC[i] = flux_SiIII[i][index_max_flux_SiIII[i]] / flux_CIII[i][index_max_flux_CIII[i]]
+    CO[i] = flux_CIII[i][index_max_flux_CIII[i]] / flux_OIII[i][index_max_flux_OIII[i]]
+    OHe[i] = flux_OIII[i][index_max_flux_OIII[i]] / flux_HeII[i][index_max_flux_HeII[i]]
+    CHe[i] = flux_CIII[i][index_max_flux_CIII[i]] / flux_HeII[i][index_max_flux_HeII[i]]
+
+figNr = 0
+
+figure = plt.figure(figNr)
+# plt.figure(figNr)
+plt.plot(SiC[0], CO[0], 'ob', label='88')
+plt.plot(SiC[1], CO[1], 'ok', label='204')
+plt.plot(SiC[2], CO[2], 'og', label='435')
+plt.plot(SiC[3], CO[3], 'or', label='22429')
+plt.grid(True)
+plt.legend(loc='best')
+plt.xlabel(r'SiIII]/CIII]')
+plt.ylabel(r'CIII]/OIII]')
+plt.savefig('plots/ALL_PART6/lineratio_SiC_CO.pdf')
+plt.clf()
+figNr += 1
+
+figure = plt.figure(figNr)
+# plt.figure(figNr)
+plt.plot(OHe[0], CHe[0], 'ob', label='88')
+plt.plot(OHe[1], CHe[1], 'ok', label='204')
+plt.plot(OHe[2], CHe[2], 'og', label='435')
+plt.plot(OHe[3], CHe[3], 'or', label='22429')
+plt.grid(True)
+plt.legend(loc='best')
+plt.xlabel(r'OIII]/HeII]')
+plt.ylabel(r'CIII]/HeII]')
+plt.savefig('plots/ALL_PART6/lineratio_OHe_CHe.pdf')
+plt.clf()
+figNr += 1
+
+# END
+########################
+print('finished part 6')
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
