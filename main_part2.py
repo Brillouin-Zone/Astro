db5b24f17f1521375e3f346714ff315ddb3cca6f
Brillouin-Zone <45795787+Brillouin-Zone@users.noreply.github.com>
2021-04-30 18:00:07 +0200

    Update main_part2.py
    - extract the wavelength range and data points for  peaks {Si III], O III], C IV, C III]} for all targets that are selected in main_part1.py

--- main_part2.py
+++ main_part2.py
@@ -36,7 +36,9 @@ import os
 #niceValue = os.nice(19)	#  nice: value in [-20 important, 19 unimportant]
 start_time = time.monotonic()
 ##########################################################################################################################
-########## P A R T    2  : SELECTION OF TARGET GALAXIES ACCORDING TO He-II PEAK GAUSSIAN FIT  / Bootstrap ################
+########## P A R T    2  : SELECTION OF TARGET GALAXIES ACCORDING TO He-II PEAK GAUSSIAN FIT #############################
+	# select and save in separate tables also the peaks for CIII], SiIII], OIII] and CIV for later
+# https://lmfit.github.io/lmfit-py/
 NAME = 'EXTRACTION'
 EXTRACTIONS = '1D_SPECTRUM_ALL_%s.fits'%(NAME)
 extractions = pyfits.open(EXTRACTIONS)
@@ -54,39 +56,57 @@ z_min = 2.86
 z_max = 4.7
 Ly_alpha_rest = 1215.67
 HeII = 1640.42
+CIII_f = 1906.68
+CIII_sf = 1908.73
+C_IV = 1548.19
+O_III_sf_1 = 1660.81
+O_III_sf_2 = 1666.15
+O_III_f = 2321.66
+SiIII_sf_1 = 1882.71
+SiIII_sf_2 = 1892.03
 
 # CHOOSE GALAXIES THAT SHOW A He-II PEAK:
 rest_vac_wavelen = [[] for j in range(select_targets)]
 flux_dens_tot = [[] for j in range(select_targets)]
 flux_dens_err = [[] for j in range(select_targets)]
-N = 10 # nr of bootstrap
 
 Lya_indices = [] # save for each galaxy the list-indices of the wavelengths corresponding to the Lya peak
 rest_wavelen_Lya = [[] for i in range(select_targets)]
 flux_Lya = [[] for i in range(select_targets)]
 noise_Lya = [[] for i in range(select_targets)]
-residual_Lya = [[] for i in range(select_targets)]
-bootstrap_flux_Lya = [[0 for m in range(N)] for i in range(select_targets)]
-bootstrap_err_Lya = [[0 for m in range(N)] for i in range(select_targets)]
-av_flux_estim_Lya = [[] for j in range(select_targets)]
-av_err_estim_Lya = [[] for j in range(select_targets)]
-tot_av_flux_Lya = [[] for j in range(select_targets)]
-tot_av_err_Lya = [[] for j in range(select_targets)]
 
 HeII_indices = [] # save here for each galaxy the list-indices of the wavelengths corresponding to the He-II peak
 rest_wavelen_HeII = [[] for j in range(select_targets)]
 flux_HeII = [[] for j in range(select_targets)]
 noise_HeII = [[] for j in range(select_targets)]
-residual_HeII = [[] for i in range(select_targets)]
-bootstrap_flux_HeII = [[0 for m in range(N)] for i in range(select_targets)]
-bootstrap_err_HeII = [[0 for m in range(N)] for i in range(select_targets)]
-av_flux_estim_HeII = [[] for j in range(select_targets)]
-av_err_estim_HeII = [[] for j in range(select_targets)]
-tot_av_flux_HeII = [[] for j in range(select_targets)]
-tot_av_err_HeII = [[] for j in range(select_targets)]
 
-columnsHe = []
+CIII_sf_indices = [] # save here for each galaxy the list-indices of the wavelengths corresponding to the CIII] peak
+rest_wavelen_CIII_sf = [[] for j in range(select_targets)]
+flux_CIII_sf = [[] for j in range(select_targets)]
+noise_CIII_sf = [[] for j in range(select_targets)]
+
+O_III_sf_1_indices = [] # save here for each galaxy the list-indices of the wavelengths corresponding to the He-II peak
+rest_wavelen_O_III_sf_1 = [[] for j in range(select_targets)]
+flux_O_III_sf_1 = [[] for j in range(select_targets)]
+noise_O_III_sf_1 = [[] for j in range(select_targets)]
+
+C_IV_indices = [] # save here for each galaxy the list-indices of the wavelengths corresponding to the He-II peak
+rest_wavelen_C_IV = [[] for j in range(select_targets)]
+flux_C_IV = [[] for j in range(select_targets)]
+noise_C_IV = [[] for j in range(select_targets)]
+
+SiIII_sf_1_indices = [] # save here for each galaxy the list-indices of the wavelengths corresponding to the He-II peak
+rest_wavelen_SiIII_sf_1 = [[] for j in range(select_targets)]
+flux_SiIII_sf_1 = [[] for j in range(select_targets)]
+noise_SiIII_sf_1 = [[] for j in range(select_targets)]
+
 columnsLya = []
+columnsHe = []
+columnsCIII = []
+columnsOIII = []
+columnsCIV = []
+columnsSiIII = []
+
 figNr = 0
 for i in range(select_targets):
 	target_nr = str(target_identification[i]) # ok
@@ -104,6 +124,26 @@ for i in range(select_targets):
 	noise_HeII[i] = np.array(flux_dens_err[i])[HeII_indices[i]]
 	rest_wavelen_HeII[i] = np.array(rest_vac_wavelen[i])[HeII_indices[i]]
 
+	CIII_sf_indices.append(np.where((rest_vac_wavelen[i] > 1902) & (rest_vac_wavelen[i] < 1916))) # TODO
+	flux_CIII_sf[i] = np.array(flux_dens_tot[i])[CIII_sf_indices[i]]
+	noise_CIII_sf[i] = np.array(flux_dens_err[i])[CIII_sf_indices[i]]
+	rest_wavelen_CIII_sf[i] = np.array(rest_vac_wavelen[i])[CIII_sf_indices[i]]
+
+	O_III_sf_1_indices.append(np.where((rest_vac_wavelen[i] > 1654) & (rest_vac_wavelen[i] < 1668)))
+	flux_O_III_sf_1[i] = np.array(flux_dens_tot[i])[O_III_sf_1_indices[i]]
+	noise_O_III_sf_1[i] = np.array(flux_dens_err[i])[O_III_sf_1_indices[i]]
+	rest_wavelen_O_III_sf_1[i] = np.array(rest_vac_wavelen[i])[O_III_sf_1_indices[i]]
+
+	C_IV_indices.append(np.where((rest_vac_wavelen[i] > 1541) & (rest_vac_wavelen[i] < 1555)))
+	flux_C_IV[i] = np.array(flux_dens_tot[i])[C_IV_indices[i]]
+	noise_C_IV[i] = np.array(flux_dens_err[i])[C_IV_indices[i]]
+	rest_wavelen_C_IV[i] = np.array(rest_vac_wavelen[i])[C_IV_indices[i]]
+
+	SiIII_sf_1_indices.append(np.where((rest_vac_wavelen[i] > 1876) & (rest_vac_wavelen[i] < 1890)))
+	flux_SiIII_sf_1[i] = np.array(flux_dens_tot[i])[SiIII_sf_1_indices[i]]
+	noise_SiIII_sf_1[i] = np.array(flux_dens_err[i])[SiIII_sf_1_indices[i]]
+	rest_wavelen_SiIII_sf_1[i] = np.array(rest_vac_wavelen[i])[SiIII_sf_1_indices[i]]
+
 	# SAVE THE Lya- AND HeII-PEAK INTO A FITS TABLE
 	mean_HeII = sum(rest_wavelen_HeII[i] * flux_HeII[i]) / sum(flux_HeII[i]) # e.g.: 1643.4745725917699 (last entry in array)
 	Delta_v_HeII = const.speed_of_light * (mean_HeII - HeII) / (1000 * HeII)  # velocity offset ([km/s]) between HeII = 164.0nm and detected maximum; c in m/s
@@ -115,9 +155,21 @@ for i in range(select_targets):
 		columnsHe.append(pyfits.Column(name='rest_wavelen_He-II_%s' % (target_nr), unit='Angstrom', format='E', array=rest_wavelen_HeII[i]))
 		columnsHe.append(pyfits.Column(name='flux_He-II_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=flux_HeII[i]))
 		columnsHe.append(pyfits.Column(name='noise_He-II_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=noise_HeII[i]))
+		columnsCIII.append(pyfits.Column(name='rest_wavelen_C-III_%s' % (target_nr), unit='Angstrom', format='E', array=rest_wavelen_CIII_sf[i]))
+		columnsCIII.append(pyfits.Column(name='flux_C-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=flux_CIII_sf[i]))
+		columnsCIII.append(pyfits.Column(name='noise_C-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=noise_CIII_sf[i]))
+		columnsOIII.append(pyfits.Column(name='rest_wavelen_O-III_%s' % (target_nr), unit='Angstrom', format='E', array=rest_wavelen_O_III_sf_1[i]))
+		columnsOIII.append(pyfits.Column(name='flux_O-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=flux_O_III_sf_1[i]))
+		columnsOIII.append(pyfits.Column(name='noise_O-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=noise_O_III_sf_1[i]))
+		columnsCIV.append(pyfits.Column(name='rest_wavelen_C-IV_%s' % (target_nr), unit='Angstrom', format='E', array=rest_wavelen_C_IV[i]))
+		columnsCIV.append(pyfits.Column(name='flux_C-IV_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=flux_C_IV[i]))
+		columnsCIV.append(pyfits.Column(name='noise_C-IV_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=noise_C_IV[i]))
+		columnsSiIII.append(pyfits.Column(name='rest_wavelen_Si-III_%s' % (target_nr), unit='Angstrom', format='E', array=rest_wavelen_SiIII_sf_1[i]))
+		columnsSiIII.append(pyfits.Column(name='flux_Si-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=flux_SiIII_sf_1[i]))
+		columnsSiIII.append(pyfits.Column(name='noise_Si-III_%s' % (target_nr), unit='10**-20 erg s-1 cm-2 A-1', format='E', array=noise_SiIII_sf_1[i]))
 
-		
 		# GAUSSIAN FIT:
+
 		def Gauss(x, amp, cen, sigma):
 			return amp *(1/(sigma * (np.sqrt(2*np.pi)))) * np.exp(-(x - cen) ** 2 / (2. * sigma ** 2))
 
@@ -183,11 +235,25 @@ for i in range(select_targets):
 
 ColsLya = pyfits.ColDefs(columnsLya)
 ColsHe = pyfits.ColDefs(columnsHe)
+ColsCIII = pyfits.ColDefs(columnsCIII)
+ColsOIII = pyfits.ColDefs(columnsOIII)
+ColsCIV = pyfits.ColDefs(columnsCIV)
+ColsSiIII = pyfits.ColDefs(columnsSiIII)
+
 hduLya = pyfits.BinTableHDU.from_columns(ColsLya)
 hduHe = pyfits.BinTableHDU.from_columns(ColsHe)
+hduCIII = pyfits.BinTableHDU.from_columns(ColsCIII)
+hduOIII = pyfits.BinTableHDU.from_columns(ColsOIII)
+hduCIV = pyfits.BinTableHDU.from_columns(ColsCIV)
+hduSiIII = pyfits.BinTableHDU.from_columns(ColsSiIII)
+
 # each of the following tables contains 183 target-peaks when using if-constraints in line 110 (-800 < Delta_v_HeII < 500)
 hduLya.writeto('Lya_selected_ALL_%s.fits' % (NAME), overwrite=True)
 hduHe.writeto('HeII_selected_ALL_%s.fits' % (NAME), overwrite=True)
+hduCIII.writeto('CIII_selected_ALL_%s.fits' % (NAME), overwrite=True)
+hduOIII.writeto('OIII_selected_ALL_%s.fits' % (NAME), overwrite=True)
+hduCIV.writeto('CIV_selected_ALL_%s.fits' % (NAME), overwrite=True)
+hduSiIII.writeto('SiIII_selected_ALL_%s.fits' % (NAME), overwrite=True)
 
 print('finished part 2')
 end_time = time.monotonic()
